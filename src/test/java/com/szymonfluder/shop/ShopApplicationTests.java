1558f028f640463357448978b112ba03e9ffba6c
fluderszymon <blueas.xm@gmail.com>
2025-05-24 11:03:18 +0200

    Initialize repository

---
+++ src/test/java/com/szymonfluder/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szymonfluder.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
