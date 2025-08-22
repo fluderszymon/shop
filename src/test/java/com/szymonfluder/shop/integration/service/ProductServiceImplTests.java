966a2c9a79677407418fb70eb274f4108beb042a
fluderszymon <blueas.xm@gmail.com>
2025-08-22 12:53:29 +0200

    Refactor ProductService, UserService tests

--- src/test/java/com/szymonfluder/shop/integration/service/ProductServiceImplTests.java
+++ src/test/java/com/szymonfluder/shop/integration/service/ProductServiceImplTests.java
@@ -5,7 +5,6 @@ import com.szymonfluder.shop.dto.ProductCreateDTO;
 import com.szymonfluder.shop.dto.ProductDTO;
 import com.szymonfluder.shop.entity.Product;
 import com.szymonfluder.shop.mapper.ProductMapperImpl;
-import com.szymonfluder.shop.repository.ProductRepository;
 import com.szymonfluder.shop.service.impl.ProductServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,19 +24,10 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class ProductServiceImplTests {
 
     @Autowired
-    private ProductRepository productRepository;
-
-    @Autowired
     private ProductServiceImpl productService;
 
     @Autowired
-    private ProductMapperImpl productMapperImpl;
-
-    @Test
-    void getAllProducts_shouldReturnEmptyList() {
-        List<ProductDTO> products = productService.getAllProducts();
-        assertThat(products.isEmpty()).isTrue();
-    }
+    private ProductMapperImpl productMapper;
 
     private Product addProductToDatabase() {
         ProductCreateDTO productCreateDTO = new ProductCreateDTO(
@@ -46,10 +36,16 @@ public class ProductServiceImplTests {
     }
 
     @Test
-    void getAllProducts_shouldReturnAllProducts() {
+    void getAllProducts_shouldReturnEmptyList() {
+        List<ProductDTO> products = productService.getAllProducts();
+        assertThat(products.isEmpty()).isTrue();
+    }
+
+    @Test
+    void getAllProducts_shouldReturnAllProductDTOs() {
         Product addedProduct = addProductToDatabase();
         List<ProductDTO> productDTOList = productService.getAllProducts();
-        ProductDTO expectedProductDTO = productMapperImpl.productToProductDTO(addedProduct);
+        ProductDTO expectedProductDTO = productMapper.productToProductDTO(addedProduct);
 
         assertThat(productDTOList.size()).isEqualTo(1);
         assertThat(productDTOList.contains(expectedProductDTO)).isTrue();
@@ -65,11 +61,11 @@ public class ProductServiceImplTests {
     }
 
     @Test
-    void getProductsByIdList_shouldReturnProductsHavingIdsInGivenList() {
+    void getProductsByIdList_shouldReturnProductDTOsHavingIdsInGivenList() {
         Product addedProduct = addProductToDatabase();
         List<Integer> existingIdList = List.of(addedProduct.getProductId());
 
-        ProductDTO expectedProductDTO = productMapperImpl.productToProductDTO(addedProduct);
+        ProductDTO expectedProductDTO = productMapper.productToProductDTO(addedProduct);
         List<ProductDTO> productDTOList = productService.getProductsByIdList(existingIdList);
 
         assertThat(productDTOList.size()).isEqualTo(1);
@@ -78,21 +74,22 @@ public class ProductServiceImplTests {
 
     @Test
     void getProductsByIdList_shouldReturnEmptyListWhenIdListIsEmpty() {
+        addProductToDatabase();
         List<ProductDTO> products = productService.getProductsByIdList(List.of());
         assertThat(products.isEmpty()).isTrue();
     }
 
     @Test
-    void getProductById_shouldReturnProductDTOFromDatabase() {
+    void getProductById_shouldReturnProductDTO() {
         Product addedProduct = addProductToDatabase();
         ProductDTO productDTO = productService.getProductById(addedProduct.getProductId());
-        ProductDTO expectedProductDTO = productMapperImpl.productToProductDTO(addedProduct);
+        ProductDTO expectedProductDTO = productMapper.productToProductDTO(addedProduct);
 
         assertEquals(expectedProductDTO, productDTO);
     }
 
     @Test
-    void getProductById_shouldThrowWhenProductWithGivenIdIsNotPresent() {
+    void getProductById_shouldThrowExceptionWhenProductWithGivenIdIsNotPresent() {
         int productId = 1;
 
         RuntimeException exception = assertThrows(
@@ -106,17 +103,20 @@ public class ProductServiceImplTests {
         Product addedProduct = addProductToDatabase();
         ProductDTO result = productService.getProductById(addedProduct.getProductId());
 
-        assertEquals(addedProduct, productMapperImpl.productDTOToProduct(result));
+        assertEquals(addedProduct, productMapper.productDTOToProduct(result));
     }
 
     @Test
     void deleteProductById_shouldDeleteProduct() {
         Product addedProduct = addProductToDatabase();
         int productId = addedProduct.getProductId();
-        assertThat(productRepository.findById(productId)).isPresent();
+        assertThat(productService.getProductById(productId)).isNotNull();
 
         productService.deleteProductById(productId);
-        assertThat(productRepository.findById(productId)).isEmpty();
+
+        RuntimeException exception = assertThrows(
+                RuntimeException.class, () -> productService.getProductById(productId));
+        assertEquals("Product with id: " + productId + " does not exist", exception.getMessage());
     }
 
     @Test
@@ -126,23 +126,23 @@ public class ProductServiceImplTests {
 
         Product productPassedToUpdateMethod 
             = new Product(productId, "Updated Product", "Updated Description", 100.50, 500);
+        ProductDTO expectedProductDTO = productMapper.productToProductDTO(productPassedToUpdateMethod);
+
         Product updatedProduct = productService.updateProduct(productPassedToUpdateMethod);
 
-        ProductDTO productAfterUpdate = productService.getProductById(productId);
-
-        assertThat(productAfterUpdate).isEqualTo(productMapperImpl.productToProductDTO(updatedProduct));
+        assertThat(expectedProductDTO).isEqualTo(productMapper.productToProductDTO(updatedProduct));
     }
 
     @Test
     void updateProduct_shouldThrowWhenThereIsNoProductToUpdateWithGivenId() {
         Product addedProduct = addProductToDatabase();
         int productId = addedProduct.getProductId();
-        Product productWithExistingProductId = new Product(productId, "Second Product", "First Description", 14.22, 200);
+        Product productWithNonExistingProductId = new Product(productId + 1, "Non Existing Product", "Non Existing Product Description", 14.22, 200);
 
         RuntimeException exception = assertThrows(
-                RuntimeException.class, () -> productService.updateProduct(productWithExistingProductId));
+                RuntimeException.class, () -> productService.updateProduct(productWithNonExistingProductId));
 
-        assertEquals("Product with id: " + productId + " does not exist", exception.getMessage());
+        assertEquals("Product with id: " + (productId + 1) + " does not exist", exception.getMessage());
     }
 
     @Test
@@ -163,36 +163,16 @@ public class ProductServiceImplTests {
 
     @Test
     void updateProductsStock_shouldUpdateProductsStock() {
-        Product firstProduct = new Product("First Product", "First Description", 14.22, 200);
-        Product secondProduct = new Product("Second Product", "Second Description", 44.12, 144);
-        Product savedFirstProduct = productRepository.save(firstProduct);
-        Product savedSecondProduct = productRepository.save(secondProduct);
-
-        ProductDTO firstProductDTO = productService.getProductById(savedFirstProduct.getProductId());
-        ProductDTO secondProductDTO = productService.getProductById(savedSecondProduct.getProductId());
-
-        CartItemDTO firstCartItemDTO = new CartItemDTO(1, 1, savedFirstProduct.getProductId(), 50);
-        CartItemDTO secondCartItemDTO = new CartItemDTO(2, 2, savedSecondProduct.getProductId(), 100);
+        Product addedProduct = addProductToDatabase();
+        ProductDTO addedProductDTO = productMapper.productToProductDTO(addedProduct);
+        CartItemDTO cartItemDTO = new CartItemDTO(1, 1, addedProduct.getProductId(), 50);
 
         Map<ProductDTO, CartItemDTO> productDTOCartItemDTOMap = new HashMap<>();
-        productDTOCartItemDTOMap.put(firstProductDTO, firstCartItemDTO);
-        productDTOCartItemDTOMap.put(secondProductDTO, secondCartItemDTO);
-
-       
+        productDTOCartItemDTOMap.put(addedProductDTO, cartItemDTO);
         productService.updateProductsStock(productDTOCartItemDTOMap);
 
-        Product updatedFirstProduct = productRepository.findById(savedFirstProduct.getProductId()).orElseThrow();
-        Product updatedSecondProduct = productRepository.findById(savedSecondProduct.getProductId()).orElseThrow();
+        ProductDTO updatedProductDTO = productService.getProductById(addedProduct.getProductId());
 
-        assertThat(updatedFirstProduct.getStock()).isEqualTo(200 - 50);
-        assertThat(updatedSecondProduct.getStock()).isEqualTo(144 - 100);
-        
-        assertThat(updatedFirstProduct.getName()).isEqualTo("First Product");
-        assertThat(updatedFirstProduct.getDescription()).isEqualTo("First Description");
-        assertThat(updatedFirstProduct.getPrice()).isEqualTo(14.22);
-        
-        assertThat(updatedSecondProduct.getName()).isEqualTo("Second Product");
-        assertThat(updatedSecondProduct.getDescription()).isEqualTo("Second Description");
-        assertThat(updatedSecondProduct.getPrice()).isEqualTo(44.12);
+        assertThat(updatedProductDTO.getStock()).isEqualTo(addedProductDTO.getStock() - cartItemDTO.getQuantity());
     }
 }
--- src/test/java/com/szymonfluder/shop/unit/service/UserServiceImplTest.java
+++ src/test/java/com/szymonfluder/shop/unit/service/UserServiceImplTest.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +22,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-// @DataJpaTest
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
 
